c8572501a53e078770f0c8759b04df8bdb48fd44
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-05-29 13:42:37 +0200

    Fix sliver assertion crash when sorted stock list reorders

--- portfolio/flutter_service_worker.js
+++ portfolio/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1f857939257db64314a5844dd323a58f",
+const RESOURCES = {"flutter_bootstrap.js": "fe86dbcc889b3a1b1a22905442ceda33",
 "version.json": "b99a7da9e2b9892004c5f1fd36282a0d",
 "index.html": "db17e5134df0f4dc6fba5eb310360eaf",
 "/": "db17e5134df0f4dc6fba5eb310360eaf",
 "privacy-policy.html": "0e88dc4e37c68763fe81ffa237bc429c",
-"main.dart.js": "5e4d687668982d195aba7d0d709e83bc",
+"main.dart.js": "361d8afa403ee103700672723426af98",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "7c825f1401da99bbfd4d5a4bc127280e",
 "icons/Icon-192.png": "92840c28204eae6192fc7eb4998846d7",
