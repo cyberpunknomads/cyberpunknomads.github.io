35ef40cbe42a403d3b210f53a6f0291e99932ed5
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-05-29 14:30:28 +0200

    Fix sold stock add: create both buy and sell transactions

--- portfolio/flutter_bootstrap.js
+++ portfolio/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "565160201"
+    serviceWorkerVersion: "1784687339"
   }
 });
