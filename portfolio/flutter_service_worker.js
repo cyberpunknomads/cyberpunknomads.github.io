5051172e866f81df4da0a2759a9180b9fef814b4
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-05-29 13:32:03 +0200

    Sort owned stocks by total P/L % descending

--- portfolio/flutter_service_worker.js
+++ portfolio/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "331fec0d6d94daaeae5c2d8d3f2dd83e",
+const RESOURCES = {"flutter_bootstrap.js": "80a47d09d02db31b4142c8d42c666f9d",
 "version.json": "b99a7da9e2b9892004c5f1fd36282a0d",
 "index.html": "db17e5134df0f4dc6fba5eb310360eaf",
 "/": "db17e5134df0f4dc6fba5eb310360eaf",
 "privacy-policy.html": "0e88dc4e37c68763fe81ffa237bc429c",
-"main.dart.js": "e44301e3e195a781d36b654a6f7acba9",
+"main.dart.js": "1fd6cf42657fa21221e10a2d1ca02a21",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "7c825f1401da99bbfd4d5a4bc127280e",
 "icons/Icon-192.png": "92840c28204eae6192fc7eb4998846d7",
