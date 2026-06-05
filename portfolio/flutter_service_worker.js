9a596ee1c07d86408e4b4e58545dfa8154f77790
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-06-05 23:45:58 +0200

    Improve contact form error reporting to show actual server error

--- portfolio/flutter_service_worker.js
+++ portfolio/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7a22330856f1b4b7db8945508d6c85a0",
+const RESOURCES = {"flutter_bootstrap.js": "f999103958b39a40fb0cb1add9fec008",
 "version.json": "b99a7da9e2b9892004c5f1fd36282a0d",
 "index.html": "db17e5134df0f4dc6fba5eb310360eaf",
 "/": "db17e5134df0f4dc6fba5eb310360eaf",
 "privacy-policy.html": "0e88dc4e37c68763fe81ffa237bc429c",
-"main.dart.js": "3826be61db15ab9bdda12eb8a1a16bcf",
+"main.dart.js": "0f49924d3206c236cb7bd1a51f2c7a63",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "7c825f1401da99bbfd4d5a4bc127280e",
 "icons/Icon-192.png": "92840c28204eae6192fc7eb4998846d7",
