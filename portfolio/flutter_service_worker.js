96c76398fe8b92fffae6705ef2bd2fd4d3c45465
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-06-07 14:32:39 +0200

    Fix auth screen platform badges to equal width

--- portfolio/flutter_service_worker.js
+++ portfolio/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f739b303f95d4d16e5a3195df382dcec",
+const RESOURCES = {"flutter_bootstrap.js": "6df22d356583330306c2b67f6bf574ec",
 "version.json": "b99a7da9e2b9892004c5f1fd36282a0d",
 "index.html": "db17e5134df0f4dc6fba5eb310360eaf",
 "/": "db17e5134df0f4dc6fba5eb310360eaf",
 "privacy-policy.html": "0e88dc4e37c68763fe81ffa237bc429c",
-"main.dart.js": "74e635aed4ef7a3d6b6f9564eed9d769",
+"main.dart.js": "ad7068a6808325eaf132b340764fc7e8",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "7c825f1401da99bbfd4d5a4bc127280e",
 "icons/Icon-192.png": "92840c28204eae6192fc7eb4998846d7",
