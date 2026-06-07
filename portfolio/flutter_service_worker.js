aa21d41c1e09b8150024e87b54abf4d41a3251b1
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-06-07 19:22:15 +0200

    Fix import: await updateStock writes sequentially to prevent transaction data loss

--- portfolio/flutter_service_worker.js
+++ portfolio/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e98c56dc832ce32b5158d702adc98879",
+const RESOURCES = {"flutter_bootstrap.js": "d6178de2bec2747e78635dc8628a228c",
 "version.json": "b99a7da9e2b9892004c5f1fd36282a0d",
 "index.html": "db17e5134df0f4dc6fba5eb310360eaf",
 "/": "db17e5134df0f4dc6fba5eb310360eaf",
 "privacy-policy.html": "0e88dc4e37c68763fe81ffa237bc429c",
-"main.dart.js": "0a7c36a3b1d98f582fc24a1218e9c817",
+"main.dart.js": "6ad390debad2b876768dbc50ce6756cc",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "7c825f1401da99bbfd4d5a4bc127280e",
 "icons/Icon-192.png": "92840c28204eae6192fc7eb4998846d7",
