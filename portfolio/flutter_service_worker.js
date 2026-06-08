77c8729648983983052c9a5bae27c61b0a73ea46
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-06-08 20:29:32 +0200

    Certificate: remove EQUIVALENT suffix from metal/commodity labels

--- portfolio/flutter_service_worker.js
+++ portfolio/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "20e72197dd0a328241f0eab62fad959e",
+const RESOURCES = {"flutter_bootstrap.js": "b60d46e9fb99dbdf5151b523ffabf782",
 "version.json": "b99a7da9e2b9892004c5f1fd36282a0d",
 "index.html": "db17e5134df0f4dc6fba5eb310360eaf",
 "/": "db17e5134df0f4dc6fba5eb310360eaf",
 "privacy-policy.html": "0e88dc4e37c68763fe81ffa237bc429c",
-"main.dart.js": "f794d8766fd7a0db6c74713fc0b4940c",
+"main.dart.js": "2f7248e77374a1b47d3c89abf39bad7b",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "7c825f1401da99bbfd4d5a4bc127280e",
 "icons/Icon-192.png": "92840c28204eae6192fc7eb4998846d7",
