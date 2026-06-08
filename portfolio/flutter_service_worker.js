cda9e8fc4392b4f27e7e22c71b23a3ae56ffcfab
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-06-08 22:04:23 +0200

    Board detail: remove EPS, fix dividend tooltip, full data audit

--- portfolio/flutter_service_worker.js
+++ portfolio/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "4736454003f7c9be2fe30d0598d92bcb",
+const RESOURCES = {"flutter_bootstrap.js": "487ceac9f4e7374c2edbca2f3f0ec239",
 "version.json": "b99a7da9e2b9892004c5f1fd36282a0d",
 "index.html": "db17e5134df0f4dc6fba5eb310360eaf",
 "/": "db17e5134df0f4dc6fba5eb310360eaf",
 "privacy-policy.html": "0e88dc4e37c68763fe81ffa237bc429c",
-"main.dart.js": "a2df9049526138bf48dede7b0eeaabcc",
+"main.dart.js": "a1cf58988736ef329806014ad3a63562",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "7c825f1401da99bbfd4d5a4bc127280e",
 "icons/Icon-192.png": "92840c28204eae6192fc7eb4998846d7",
