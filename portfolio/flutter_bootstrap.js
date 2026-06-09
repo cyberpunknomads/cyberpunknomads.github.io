d8fa9b134d7e940a1847029ac1659d1caba8e039
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-06-09 21:09:15 +0200

    Add Investing Rules feature + pre-store prep (bundle IDs, icons, splash, IAP)

--- portfolio/flutter_bootstrap.js
+++ portfolio/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "420140158"
+    serviceWorkerVersion: "2222549960"
   }
 });
