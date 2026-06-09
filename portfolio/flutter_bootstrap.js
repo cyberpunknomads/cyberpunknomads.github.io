dcd530e258cdb56a6a4d81d22246f5b22bcde741
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-06-09 22:07:36 +0200

    Add landing section to auth screen with features, tags, screenshots & CTA

--- portfolio/flutter_bootstrap.js
+++ portfolio/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2222549960"
+    serviceWorkerVersion: "1243241026"
   }
 });
