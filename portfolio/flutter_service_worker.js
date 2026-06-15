4f89c52bfcd19605e99c4a5f226c4a6cec6ab5fd
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-06-15 20:24:44 +0200

    Add dividend history bar chart to Portfolio Quality card

--- portfolio/flutter_service_worker.js
+++ portfolio/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9da7708417fc2e8a77a446b8d916ab9a",
+const RESOURCES = {"flutter_bootstrap.js": "7416ef72b89c2e0294f44388239b5fdb",
 "version.json": "ea741ce5224ab02c5a0147385f83a0d2",
 "index.html": "db17e5134df0f4dc6fba5eb310360eaf",
 "/": "db17e5134df0f4dc6fba5eb310360eaf",
 "privacy-policy.html": "89d57ecdaa62a7f4ff7b171ef0a278fb",
-"main.dart.js": "49af96a34c3497e8ef7dd69723512160",
+"main.dart.js": "fc1d1e9f5c23f090b30b529ad80b4c67",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "7c825f1401da99bbfd4d5a4bc127280e",
 "icons/Icon-192.png": "92840c28204eae6192fc7eb4998846d7",
