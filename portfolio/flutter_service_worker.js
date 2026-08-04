8f7fbca541c9dabb1cfabb448f2b56a9f9f2a94b
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-08-04 20:22:21 +0200

    Deploy Tier 1 cash-flow ledger (deposits/withdrawals)

--- portfolio/flutter_service_worker.js
+++ portfolio/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8ba8adad080382f7373c02b74e9694c3",
+const RESOURCES = {"flutter_bootstrap.js": "da0ea1b31261ae4c4804f590abbe3bd2",
 "version.json": "ea741ce5224ab02c5a0147385f83a0d2",
 "index.html": "a43abfa775e7fc5947ac0d19e9544153",
 "/": "a43abfa775e7fc5947ac0d19e9544153",
 "privacy-policy.html": "89d57ecdaa62a7f4ff7b171ef0a278fb",
-"main.dart.js": "e511cbe44a528a57efc03c1bd6a7131b",
+"main.dart.js": "4389ef574271f5dc174792d544664075",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "7c825f1401da99bbfd4d5a4bc127280e",
 "icons/Icon-192.png": "92840c28204eae6192fc7eb4998846d7",
@@ -24,7 +24,7 @@ const RESOURCES = {"flutter_bootstrap.js": "8ba8adad080382f7373c02b74e9694c3",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "8acddf57a7608f8a3d1a60d57c37919f",
-"assets/fonts/MaterialIcons-Regular.otf": "e02d30e3007fbc77eb604e896a087679",
+"assets/fonts/MaterialIcons-Regular.otf": "a758ddf36cc0c9cc87bf16500f2d7d03",
 "assets/assets/screenshots/portfolio_summary.png": "b6a4afd1e488cd3329d8ea7468471638",
 "assets/assets/screenshots/stock_2.png": "aa8edfdd7d6da99d218b26449cf7fe8d",
 "assets/assets/screenshots/stock.png": "17754989a5c771b70869af2fb65d195d",
