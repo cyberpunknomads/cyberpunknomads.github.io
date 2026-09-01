12334372472f57e9e8eb3f7880e44f70cea81af2
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-09-01 11:05:39 +0200

    Deploy Portfolio Keeper: Screener filter GUI polish (source 05005c5)

--- portfolio/flutter_service_worker.js
+++ portfolio/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "09eb6ba04db2e3564ae0342dc39aa3cb",
+const RESOURCES = {"flutter_bootstrap.js": "3666db43e2609a2b7450eb24b46679cf",
 "version.json": "ea741ce5224ab02c5a0147385f83a0d2",
 "index.html": "a43abfa775e7fc5947ac0d19e9544153",
 "/": "a43abfa775e7fc5947ac0d19e9544153",
 "privacy-policy.html": "a8cd0f7ff495c020f2e08ad977cd650a",
-"main.dart.js": "c8706226687275b26c27a869a400ee1a",
+"main.dart.js": "2230f66b08a27a13588a916e77c24ddb",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "7c825f1401da99bbfd4d5a4bc127280e",
 "icons/Icon-192.png": "92840c28204eae6192fc7eb4998846d7",
