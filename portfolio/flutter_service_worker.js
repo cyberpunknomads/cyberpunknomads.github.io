21afd7bdff0b5ffe4de74ede02cd2597eb4f16a6
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-09-16 18:45:23 +0200

    Portfolio: company descriptions on the holding screen

--- portfolio/flutter_service_worker.js
+++ portfolio/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3a4202b1293d6791450f30f80fd04a48",
+const RESOURCES = {"flutter_bootstrap.js": "55f62394329c3fbcdc040a6cd56d2e13",
 "version.json": "ea741ce5224ab02c5a0147385f83a0d2",
 "index.html": "a43abfa775e7fc5947ac0d19e9544153",
 "/": "a43abfa775e7fc5947ac0d19e9544153",
 "privacy-policy.html": "a8cd0f7ff495c020f2e08ad977cd650a",
-"main.dart.js": "25af7f9bcc71d925c9cea53c79a8e574",
+"main.dart.js": "b5ac733c03eaba090e8b65c129783e10",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "7c825f1401da99bbfd4d5a4bc127280e",
 "icons/Icon-192.png": "92840c28204eae6192fc7eb4998846d7",
