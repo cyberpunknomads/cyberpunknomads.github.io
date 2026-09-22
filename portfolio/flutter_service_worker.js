a35c641b0e3623e1ac2e272ddaea3b005a6ab1e4
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-09-22 22:08:25 +0200

    Portfolio Keeper: item 6, dividends in the currency they were received in (source 1e527e5)

--- portfolio/flutter_service_worker.js
+++ portfolio/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "93d5983685be81549596cfed20bc49c9",
+const RESOURCES = {"flutter_bootstrap.js": "221d50f70238cb5705acfca917a10d7c",
 "version.json": "ea741ce5224ab02c5a0147385f83a0d2",
 "index.html": "a43abfa775e7fc5947ac0d19e9544153",
 "/": "a43abfa775e7fc5947ac0d19e9544153",
 "privacy-policy.html": "a8cd0f7ff495c020f2e08ad977cd650a",
-"main.dart.js": "78092a159d087cc4269e6a4adaeb0914",
+"main.dart.js": "0b7933db1d4217d3390c1ec07fa78395",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "7c825f1401da99bbfd4d5a4bc127280e",
 "icons/Icon-192.png": "92840c28204eae6192fc7eb4998846d7",
