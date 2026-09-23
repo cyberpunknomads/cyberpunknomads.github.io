7d3f6b38e4c3c153c37e7968d2309fd6a7b0279d
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-09-23 19:05:32 +0200

    Portfolio Keeper: Google figures refresh again (source 4feae2d)

--- portfolio/flutter_bootstrap.js
+++ portfolio/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "529727594"
+    serviceWorkerVersion: "1724296604"
   }
 });
