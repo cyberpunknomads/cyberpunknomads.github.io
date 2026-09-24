e7e55363276acfb09b03c48b5baea8af8323064c
CyberPunk Nomads <90918872+cyberpunknomads@users.noreply.github.com>
2026-09-24 19:19:58 +0200

    Portfolio Keeper: chart library updated to fl_chart 1.2.0, no visible change (source 6e7d021)

--- portfolio/flutter_bootstrap.js
+++ portfolio/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1807622110"
+    serviceWorkerVersion: "3730600967"
   }
 });
